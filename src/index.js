17d8866372aae2136360122328b1afb34339e416
Mihir Samant <47722466+miSamant@users.noreply.github.com>
2025-08-01 14:48:47 -0400

    Create src/index.js with initial React setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AirflowConfigCalculator from './Airflow-Calculator';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AirflowConfigCalculator />
+  </React.StrictMode>
+);
